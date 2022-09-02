bf85c4b4b7e6a6daf88d342d000d99cc3128827e
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2022-09-02 16:08:58 -0400

    Creación de proyecto

---
+++ src/main/java/ucb/edu/bo/tallersoftware/TallersoftwareApplication.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.tallersoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallersoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallersoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucb/edu/bo/tallersoftware/TallersoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.tallersoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallersoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
