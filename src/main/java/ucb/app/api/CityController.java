1f1640e0e74ed60531569315987da9c558d381e6
RONALDO ROJAS AYALA <ronaldorojasayala@192.168.0.103>
2022-09-04 21:20:25 -0400

    agregado de archivos

--- src/main/java/ucb/app/api/CityController.java
+++ src/main/java/ucb/app/api/CityController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +31,30 @@ public class CityController {
     public ResponseEntity<List<City>> getCities() {
         List<City> cities = cityRepository.findAll();
         return new ResponseEntity<>(cities, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{cityId}")
+    public ResponseEntity<City> getCityById(@PathVariable("cityId") Integer cityId) throws Exception {
+        City city = cityRepository.findById(cityId).orElseThrow(() -> new Exception("Could not find city"));
+        return new ResponseEntity<>(city, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<City> postCity(@RequestBody City city) {
+        City cityReponse = cityRepository.save(city);
+        return new ResponseEntity<>(cityReponse, HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{cityId}")
+    public ResponseEntity<City> putCity(@PathVariable("cityId") Integer cityId, @RequestBody City city) {
+        City cityFound = cityRepository.getReferenceById(cityId);
+        City cityResponse = cityRepository.save(cityFound);
+        return new ResponseEntity<>(cityResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{cityId}")
+    public ResponseEntity<String> deleteCity(@PathVariable("cityId") Integer cityId) {
+        cityRepository.deleteById(cityId);
+        return new ResponseEntity<>("City deleted successfully", HttpStatus.OK);
     }
 }
