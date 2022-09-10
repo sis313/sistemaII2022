ca3aaa8be1282f6f7d76f9208048cdc744aa2193
RONALDO ROJAS AYALA <ronaldorojasayala@192.168.0.101>
2022-09-10 04:44:17 -0400

    agregado de archivos

--- src/main/java/ucb/app/AppApplication.java
+++ src/main/java/ucb/app/AppApplication.java
@@ -1,16 +1,10 @@
 package ucb.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ucb.app.repository.CityRepository;
-
 @SpringBootApplication
 public class AppApplication {
-	@Autowired
-	private CityRepository cityRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
