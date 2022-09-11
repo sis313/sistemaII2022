dc8f5fa3b63c9831279f0a352cb32d5f90017dae
RONALDO ROJAS AYALA <ronaldorojasayala@192.168.0.101>
2022-09-11 14:28:45 -0400

    agregado de archivos

--- src/main/java/ucb/app/controller/BusinessController.java
+++ src/main/java/ucb/app/controller/BusinessController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import ucb.app.dto.BusinessDto;
 import ucb.app.model.Business;
 import ucb.app.service.BusinessService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/business")
 public class BusinessController {
