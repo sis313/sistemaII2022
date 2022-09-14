8514735cbbbf7ef0eadda3d72e5698964ccc0ecc
RONALDO ROJAS AYALA <ronaldorojasayala@192.168.0.105>
2022-09-13 22:31:41 -0400

    agregado de archivos

--- src/main/java/ucb/app/repository/BranchRepository.java
+++ src/main/java/ucb/app/repository/BranchRepository.java
@@ -1,11 +1,9 @@
 package ucb.app.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ucb.app.model.Branch;
 
 public interface BranchRepository extends JpaRepository<Branch, Integer> {
-    public List<Branch> findByIdBusiness(Integer idBusiness);
+
 }
