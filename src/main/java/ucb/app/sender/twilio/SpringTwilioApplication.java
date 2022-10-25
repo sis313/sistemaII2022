fe9ec2258b8d1156e4becc4605e099399aca4a6c
Mauro Moya <99930598+MauroMoyaUCB@users.noreply.github.com>
2022-10-24 23:49:28 -0400

    Add: Twilio

---
+++ src/main/java/ucb/app/sender/twilio/SpringTwilioApplication.java
@@ -0,0 +1,15 @@
+package ucb.app.sender.twilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringTwilioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTwilioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ucb/SpringTwilioApplicationTests.java
@@ -0,0 +1,12 @@
+package ucb;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTwilioApplicationTests {
+
+    @org.junit.Test
+    void contextLoads() {
+    }
+
+}
